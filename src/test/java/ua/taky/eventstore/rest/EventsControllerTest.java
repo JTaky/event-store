f96ec7c841d6adcb4344ed7e7e375b85af9f053f
Alex Nitavsky <Alex.Nitavsky@grammarly.com>
2013-10-13 23:46:17 +0300

    Use junit instead of testNG

---
+++ src/test/java/ua/taky/eventstore/rest/EventsControllerTest.java
@@ -0,0 +1,14 @@
+package ua.taky.eventstore.rest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class EventsControllerTest {
+	
+	@Test
+	public void testEventsSearch(){
+		Assert.assertTrue(true);
+	}
+
+}
